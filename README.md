287e2002aa12581f33d4fbc9a440a01edd2240b2
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-01-08 10:54:43 -0600

    initialize api server

---
+++ README.md
@@ -0,0 +1,1 @@
+# A backend for various apis I use like Dark Sky, Google Maps API, etc.
---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const darkSkyApiKey = process.env.darkSkyApiKey;
+const googleMapsApiKey = process.env.googleMapsApiKey;
+const openCageApiKey = process.env.openCageApiKey;
+
+app.use(express.json());
+app.use(
+    cors({
+        origin: '*'
+    })
+);
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+    console.log(`shinkhouse apis listening on port ${port}`);
+});
+
+app.get('/', async(req, res) => {
+    res.json({status: 'App is up and running'})
+});
+
+app.get('/forecast', async(req, res) => {
+    const params = req.query;
+
+    axios
+        .get(
+            `https://api.darksky.net/forecast/${darkSkyApiKey}/${params.latitude},${params.longitude}`
+        )
+        .then((response) => {
+            console.log(response.data);
+            console.log(response.status);
+            res.json(response.data);
+        })
+    
+});
+
+app.get('/maps/api/place/autocomplete/json', async (req, res) => {
+    const params = req.query;
+    const apiUrl =
+        `https://maps.googleapis.com/maps/api/place/autocomplete/json?` +
+        `input=${params.input}&types=${params.types}&key=${googleMapsApiKey}`;
+    axios
+        .get(
+            apiUrl
+        )
+        .then((response) => {
+            console.log(apiUrl);
+            console.log(response.data);
+            console.log(response.status);
+            res.json(response.data);
+        });
+});
+
+app.get('/maps/api/geocode/json', async (req, res) => {
+    const params = req.query;
+    const apiUrl =
+        `https://maps.googleapis.com/maps/api/geocode/json?` +
+        `address=${params.address}&key=${googleMapsApiKey}`;
+    axios
+        .get(
+            apiUrl
+        )
+        .then((response) => {
+            // key=${googleMapsApiKey}
+            console.log(apiUrl);
+            // console.log(response.data);
+            // console.log(response.status);
+            res.json(response.data);
+        });
+});
+
+app.get('/geocode/v1/json', async (req, res) => {
+    const params = req.query;
+    axios
+        .get(
+            `https://api.opencagedata.com/geocode/v1/json?q=` +
+                `=${params.address}&key=${openCageApiKey}`
+        )
+        .then((response) => {
+            console.log(response.data);
+            console.log(response.status);
+            res.json(response.data);
+        });
+});
