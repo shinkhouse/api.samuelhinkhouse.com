0dff4c04a6e205406c1d42abab8c87065653ed25
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-01-12 12:04:52 -0600

    add giphy and dictionary apis

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const darkSkyApiKey = process.env.darkSkyApiKey;
 const googleMapsApiKey =
     process.env.googleMapsApiKey;
 const openCageApiKey = process.env.openCageApiKey;
+const wordsApiKey =
+    process.env.wordsApiKey;
+const giphyApiKey =
+    process.env.giphyApiKey;
 
 app.use(express.json());
 app.use(
@@ -24,6 +28,28 @@ app.get('/', async(req, res) => {
     res.json({status: 'App is up and running'})
 });
 
+app.get('/words/:word', async(req, res) => {
+    console.log(req.params, req.query);
+    var options = {
+        method: 'GET',
+        url: `https://wordsapiv1.p.rapidapi.com/words/${req.params.word}`,
+        headers: {
+            'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com',
+            'x-rapidapi-key':
+                wordsApiKey
+        }
+    };
+
+    axios
+        .request(options)
+        .then(function (response) {
+            res.json(response.data);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+})
+
 app.get('/forecast', async(req, res) => {
     const params = req.query;
 
@@ -37,6 +63,17 @@ app.get('/forecast', async(req, res) => {
             res.json(response.data);
         })
     
+});
+
+app.get('/gifs/search', async(req, res) => {
+    const apiUrl = `https://api.giphy.com/v1/gifs/search?q=${req.query.q}%20&api_key=${giphyApiKey}&limit=50`;;
+
+    axios.get(apiUrl).then((response) => {
+        console.log(apiUrl);
+        console.log(response.data);
+        console.log(response.status);
+        res.json(response.data);
+    });
 });
 
 app.get('/maps/api/place/autocomplete/json', async (req, res) => {
